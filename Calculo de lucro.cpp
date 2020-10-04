53e6aed605702d3f9d1b7a89c23a32979b2fc6a9
Gabriel Dos S N Martins <50385858+spektremkev@users.noreply.github.com>
2020-10-04 19:47:59 -0300

    Add files via upload

---
+++ Calculo de lucro.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+ATIVIDADE I  - QUESTÃO 6
+
+*/
+
+int main(void)<%
+
+    float preco;
+
+    printf("Entre com o PRECO DE COMPRA.\n"); scanf("%f",&preco);
+
+    if ( preco < 30 ) <% printf("VALOR DE VENDA : %.2f\n", preco * 1.35); %>
+
+	else <% printf( "VALOR DE VENDA : %.2f\n", preco * 1.40); %>
+
+    return(0);
+%>
